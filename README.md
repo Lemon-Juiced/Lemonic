a99c689fe5de57924c0d77d3b78a9903d432eb34
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-03-18 20:18:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lemonic C
+
+Lemonic C, or simply Lemonic, is a new superset of C designed to simplify and enhance the language.
+
+## What is Lemonic?
+The name "Lemonic" combines "Lemon" (inspired by the developer) and "Demonic" (reflecting the challenging nature of creating this language). Although the full name is Lemonic C, it is often abbreviated to just Lemonic.
+
+## How it Works
+Lemonic relies on existing C tools. When a Lemonic program is compiled, it is first translated into an equivalent C program, which is then compiled using a standard C compiler.
+
+### Compiler
+The Lemonic compiler, `lemonicomp`, is nowhere near started.
+
+## Completed
+- `ArrayList.c` and `ArrayListTest.c`
+
+## To Do:
+- `lemonicomp` Compiler
+- New C Implementations of existing C++ features, or equivalents. 
---
+++ dev/lemonjuice/lemonic/ArrayList.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * The ArrayList "class" is a dynamic array that can store any type of data.
+ * The class is implemented as a struct that contains a few fields:
+ * - data: A pointer to the array of void pointers that store the data. 
+ *         void* is a generic pointer type. It can point to any type of data.
+ * - size: The number of elements currently stored in the array.
+ * - capacity: The maximum number of elements that the array can store.
+ * - growth_factor: The factor by which the array's capacity is increased when it is reallocated.
+ * 
+ * The class also contains a few functions to manipulate the array:
+ * - createArrayList: Creates a new ArrayList with a given capacity.
+ * - destroyArrayList: Frees the memory used by the ArrayList.
+ * - addElement: Adds an element to the end of the ArrayList.
+ * - removeElement: Removes an element from the ArrayList.
+ * - get: Gets an element from the ArrayList.
+ * - set: Sets an element in the ArrayList.
+ * - size: Returns the number of elements in the ArrayList.
+ * - capacity: Returns the capacity of the ArrayList.
+ * 
+ * When the capacity of the ArrayList is reached, the array is reallocated with a larger capacity.
+ * The new capacity is calculated as the old capacity multiplied by a growth factor (e.g., 2).
+ */
+typedef struct {
+    // Data Members
+    void** data;
+    int size;
+    int capacity;
+    int growth_factor;
+
+    // Member Functions
+    void (*add)(void* self, void* element);
+    void (*removeElement)(void* self, int index);
+    void* (*get)(void* self, int index);
+    void (*set)(void* self, int index, void* element);
+    int (*getSize)(void* self);
+    int (*getCapacity)(void* self);
+} ArrayList;
+
+// Function Prototypes
+ArrayList* createArrayList(int capacity, int growth_factor);
+ArrayList* newArrayList();
+void destroyArrayList(void* self);
+void add(void* self, void* element);
+void removeElement(void* self, int index);
+void* get(void* self, int index);
+void set(void* self, int index, void* element);
+int getSize(void* self);
+int getCapacity(void* self);
