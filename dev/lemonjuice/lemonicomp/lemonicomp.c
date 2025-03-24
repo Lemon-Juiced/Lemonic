8b7261d0763d39eeef5a95a17f782cd2920335ed
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-03-24 13:56:49 -0400

    Added -c flag to compiler - not fully implemented

--- dev/lemonjuice/lemonicomp/lemonicomp.c
+++ dev/lemonjuice/lemonicomp/lemonicomp.c
@@ -45,13 +45,18 @@ int isValidLemonicFile(const char *filename) {
  * The compiler in this case is more of a translator that translates Lemonic code to C code.
  * This compiler will continue to evolve with the Lemonic programming language.
  * 
+ * Additional Arguments:
+ * -c: The generated C code will not be deleted after compilation. (Default behavior is to delete the C code after compilation.)
+ * 
  * Steps:
  * 1. Check if the correct number of arguments is provided.
- * 2. Check if the input file is a valid Lemonic file.
+ * 2. Check if the -c flag is provided.
+ * 3. Check if the input file is a valid Lemonic file.
  * ...
  * Unknown Step. Add as EOL comments in the C file that reference the line from the Lemonic file, for error handling.
  * ...
- * Last. Hand the newly created C file to the C compiler.
+ * n-1. If the -c flag is not provided, delete the C file after compilation.
+ * n. Hand the newly created C file to the C compiler.
  * 
  * @argc The number of command line arguments.
  * @argv The command line arguments.
@@ -59,18 +64,50 @@ int isValidLemonicFile(const char *filename) {
  */
 int main(int argc, char *argv[]) {
     // Check if the correct number of arguments is provided
-    // This is naively done for now; this could easily chnage when Lemonic supports more than one file.
-    if (argc != 2) {
+    if (argc < 2 || argc > 3) {
         // Get the name of the program from argv[0], but remove the path from it.
         char *programName = removePath(argv[0]);
-        fprintf(stderr, "Usage: %s <input_file.lmc>\n", programName);
+        fprintf(stderr, "Usage: %s <input_file.lmc> [-c]\n", programName);
         return 1;
+    }
+
+    // Check if the -c flag is provided
+    int keepCFile = 0;
+    if (argc == 3 && strcmp(argv[2], "-c") == 0) {
+        keepCFile = 1;
     }
 
     // Check if the input file is a valid Lemonic file
     if (!isValidLemonicFile(argv[1])) {
         fprintf(stderr, "Error: Invalid Lemonic file.\nPlease provide a .lmc file.\n");
         return 1;
+    }
+
+    // Create a new C file with the same name as the input file, but with a .c extension
+    char *inputFileName = argv[1];
+    char *outputFileName = (char *)malloc(strlen(inputFileName) + 2); // +2 for .c and null terminator
+    strcpy(outputFileName, inputFileName);
+    char *extension = strrchr(outputFileName, '.');
+    if (extension != NULL) {
+        *extension = '\0'; // Remove the old extension
+    }
+    strcat(outputFileName, ".c"); // Add the new extension
+    FILE *outputFile = fopen(outputFileName, "w");
+    if (outputFile == NULL) {
+        fprintf(stderr, "Error: Could not create output file.\n");
+        free(outputFileName);
+        return 1;
+    }
+
+    // If the -c flag is not provided, delete the C file after compilation
+    if (!keepCFile) {
+        // Delete the C file after compilation
+        if (remove(outputFileName) != 0) {
+            fprintf(stderr, "Error: Could not delete output file.\n"); // I'm currently getting this when trying to deleter so unexpected bug, needs more time to fix though.
+            fclose(outputFile);
+            free(outputFileName);
+            return 1;
+        }
     }
 
     return 0; // Success
